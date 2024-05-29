5fc9075503167e36829c7d6824e7f8feac380c32
Andres Amaya <andres.amaya@sinapsis.global>
2024-05-29 16:47:59 -0500

    feat: Finish layout main components

--- src/views/MyAlbum.tsx
+++ src/views/MyAlbum.tsx
@@ -1,3 +1,22 @@
+import { Characters } from "../components/myAlbum/Characters";
+import { Movies } from "../components/myAlbum/Movies";
+import { Ships } from "../components/myAlbum/Ships";
+
 export const MyAlbum = () => {
-  return <div>MyAlbum</div>;
+  return (
+    <div className="container">
+      <div className="z-1 position-relative">
+        <h6 className="text-danger">Instrucciones:</h6>
+        <p className="text-white">
+          El album se almacena en localStorage, si desean reiniciar el album se
+          debe eliminar las variable del localStorage y refrescar la pagina! :D
+        </p>
+      </div>
+      <Movies />
+      <hr />
+      <Characters />
+      <hr />
+      <Ships />
+    </div>
+  );
 };
